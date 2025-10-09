38b37dad3650f0cea9387dd78e8991e17ebae0ad
Odunlami Zacchaeus <odunlamizacchaeus@gmail.com>
2025-10-09 17:55:17 +0100

    revert to findAndRegisterModules

--- src/main/java/io/github/odunlamizo/jsonbin/util/JsonUtil.java
+++ src/main/java/io/github/odunlamizo/jsonbin/util/JsonUtil.java
@@ -3,19 +3,13 @@ package io.github.odunlamizo.jsonbin.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public final class JsonUtil {
 
     private static final ObjectMapper MAPPER;
 
     static {
-        MAPPER =
-                new ObjectMapper()
-                        .registerModule(new JavaTimeModule())
-                        .findAndRegisterModules()
-                        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        MAPPER = new ObjectMapper().findAndRegisterModules();
     }
 
     private JsonUtil() {}
