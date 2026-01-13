69202d861358d395a204102a83164eae98d2c646
Odunlami Zacchaeus <odunlamizacchaeus@gmail.com>
2026-01-13 13:37:00 +0100

    fix error model (#16)

--- src/main/java/io/github/odunlamizo/jsonbin/model/Error.java
+++ src/main/java/io/github/odunlamizo/jsonbin/model/Error.java
@@ -1,10 +1,12 @@
 package io.github.odunlamizo.jsonbin.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Error {
 
     private String message;
